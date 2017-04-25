ed167f68bf5fec9100e41a5c6efbe06c40a95abb
Ipkcle <Ipkcle@gmail.com>
2017-04-25 11:17:29 -0500

    Create Level.java

---
+++ Game/src/main/Level.java
@@ -0,0 +1,17 @@
+package main;
+
+//its this easy
+
+
+public class Level {
+
+ArrayList<Platform> platforms;
+	
+	 public (ArrayList<Platform> plats){
+	     	this.platforms = plats
+	 }
+
+	public void render(){
+    //render
+  }
+}
