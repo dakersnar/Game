aca1b6448dba4898d9d8945124b3124e5398dc34
Ipkcle <Ipkcle@gmail.com>
2017-04-25 11:35:09 -0500

    Update GameState.java

--- Game/src/main/GameState.java
+++ Game/src/main/GameState.java
@@ -19,11 +19,34 @@ public class GameState extends State{
 
 	int x=0,y=0;
 
-	
+	ArrayList<Level> levels;
 	
 	
 	public GameState(){
-
+		
+		// if/when you make a system to load levels from files or proc gen,
+		// just pass the lists of stuff into these constructors.
+		
+		/*
+		if you read from a file you can just make a for loop for each read file (contains info about whats in the level)
+		to add the info from the file to a new level and add it to levels.
+		That way nothing is hardcoded.
+		
+		Having a different static class for each level is the EPITOME of hardcoding.
+		
+		*/
+		
+		//put plats in here
+		
+		Platform[] plats1 = [];
+		Platform[] plats2 = [];
+		Platform[] plats3 = [];
+		
+		
+		//create levels
+		levels.add(new Level(Arrays.asList(plats1)));
+		levels.add(new Level(Arrays.asList(plats2)));
+		levels.add(new Level(Arrays.asList(plats3)));
 		
 	
 
@@ -38,7 +61,9 @@ public class GameState extends State{
 	
 		Character.render(g);
 		Things.render(g);
-		
+		for(Level level:levels){
+			level.render(g);
+		}
 	}
 
 	
